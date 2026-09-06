fff13f03a210295ed5b6fcfc0f2379ec5698f0fb
steve-764 <mwangisteve764@gmail.com>
2026-09-06 09:48:29 +0300

    create total value and low stock functions

--- practice_2/mini_inventory_system/inventory.py
+++ practice_2/mini_inventory_system/inventory.py
@@ -1,6 +1,7 @@
 # create empty list for inventory
 inventory = []
 
+# adding a product to the inventory
 def add_product(inventory, name, price, quantity):
     product = {
                "name":name,
@@ -9,11 +10,49 @@ def add_product(inventory, name, price, quantity):
                }
     inventory.append(product)
 
+# calculating total inventory cost
+
+def total_value(inventory):
+    total = 0
+    for product in inventory:
+        total += product["price"] * product["quantity"]
+
+    return total
+
+# finding low stock products, default 5 
+
+def low_stock(inventory, threshold = 5):
+    low_stock_products = []
+
+    for product in inventory:
+        if product["quantity"] < threshold:
+            low_stock_products.append(product)
+
+    return low_stock_products
+
+# adding 5 products
 add_product(inventory, "Sugar 1kg", 150, 20)
 add_product(inventory, "Rice 2kg", 320, 4)
 add_product(inventory, "Milk 500ml", 60, 15)
 add_product(inventory, "Bread", 70, 3)
 add_product(inventory, "Cooking Oil 1L", 400, 8)
 
+# printing inventory
+print("Current inventory")
+for product in inventory:
+    print(product)
+print("=" * 30)
+print()
 
-print(inventory)
+# printig  total inventory value
+print(f"Total inventory value : {total_value(inventory)}")
+print("=" * 30)
+print()
+
+# printing low stock products 
+print("Low stock products")
+# using for loop to print each product on a new line
+for product in low_stock(inventory):
+    print(product)
+
+    
